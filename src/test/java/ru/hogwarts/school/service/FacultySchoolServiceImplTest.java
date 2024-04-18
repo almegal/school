a8cdb7f868074d8c8c778c421239b33a3086eac8
alekseipeikhel <alekseipeikhel@gmail.com>
2024-04-18 10:26:47 +0300

    переписаны тесты с мокос дб, добавлено тестирование исклюений

--- src/test/java/ru/hogwarts/school/service/FacultySchoolServiceImplTest.java
+++ src/test/java/ru/hogwarts/school/service/FacultySchoolServiceImplTest.java
@@ -3,26 +3,35 @@ package ru.hogwarts.school.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repositories.FacultysRepository;
 import ru.hogwarts.school.service.implementation.FacultySchoolServiceImpl;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class FacultySchoolServiceImplTest {
-    private final FacultySchoolServiceImpl schoolServiceImpl = new FacultySchoolServiceImpl();
     private final Map<Long, Faculty> DEFAULT_MAPS_OF_FACULTY = new HashMap<>();
+    @Mock
+    private FacultysRepository facultysRepository;
+    @InjectMocks
+    private FacultySchoolServiceImpl schoolServiceImpl;
 
     @BeforeEach
     void set_up() {
         DEFAULT_MAPS_OF_FACULTY.put((long) 1, new Faculty(1, "Griffindor", "Red"));
-        DEFAULT_MAPS_OF_FACULTY.put((long) 2, new Faculty(2, "Slytherin", "Green"));
+        DEFAULT_MAPS_OF_FACULTY.put((long) 2, new Faculty(2, "Slytherin", "Red"));
         DEFAULT_MAPS_OF_FACULTY.put((long) 3, new Faculty(3, "Hufflepufe", "Yellow"));
         DEFAULT_MAPS_OF_FACULTY.put((long) 4, new Faculty(4, "Ravenclan", "Blue"));
-
-        schoolServiceImpl.create(DEFAULT_MAPS_OF_FACULTY.get((long) 1));
     }
 
 
@@ -31,11 +40,12 @@ public class FacultySchoolServiceImplTest {
     public void createFaculty() {
         // подготовка ожидаемого результата
         Faculty expected = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
+        when(facultysRepository.save(any(Faculty.class))).thenReturn(expected);
         // подготовка актуального результата
         Faculty actual = schoolServiceImpl.create(expected);
         // тест
         assertEquals(expected, actual);
-
+        verify(facultysRepository, times(1)).save(any(Faculty.class));
     }
 
     @Test
@@ -43,11 +53,27 @@ public class FacultySchoolServiceImplTest {
     public void removeFacyltu() {
         // подготовка ожидаемого результата
         Faculty expected = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
+        when(facultysRepository.existsById(anyLong())).thenReturn(true);
+        doNothing().when(facultysRepository).deleteById(anyLong());
         // подготовка актуального результата
-        Faculty actual = schoolServiceImpl.remove(1);
+        schoolServiceImpl.remove(anyLong());
         // тест
-        assertEquals(expected, actual);
-        assertNull(schoolServiceImpl.get(1));
+        verify(facultysRepository, times(1)).existsById(anyLong());
+        verify(facultysRepository, times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    @DisplayName("Исключение при удалении если нет такого факультета")
+    public void throwWhenTryRemoveFacultyIfNotExsist() {
+        // подготовка ожидаесого результата
+        Faculty expected = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
+        when(facultysRepository.existsById(anyLong())).thenReturn(false);
+        // подготовка актуального результата
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> schoolServiceImpl.remove(anyLong()));
+        // тест
+        assertEquals("Такого факультута нет в БД", thrown.getMessage());
+        verify(facultysRepository, times(1)).existsById(anyLong());
+        verify(facultysRepository, times(0)).deleteById(anyLong());
     }
 
     @Test
@@ -55,10 +81,12 @@ public class FacultySchoolServiceImplTest {
     public void getFacultyById() {
         // подготовка ожидаемого результата
         Faculty expected = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
+        when(facultysRepository.findById(anyLong())).thenReturn(Optional.of(expected));
         // подготовка актуального результата
         Faculty actual = schoolServiceImpl.get(1);
         // тест
         assertEquals(expected, actual);
+        verify(facultysRepository, times(1)).findById(anyLong());
     }
 
     @Test
@@ -66,36 +94,43 @@ public class FacultySchoolServiceImplTest {
     public void editFaculty() {
         // подготовка ожидаемого результата
         Faculty expected = new Faculty(1, "No Griffindor", "Brown");
+        when(facultysRepository.existsById(anyLong())).thenReturn(true);
+        when(facultysRepository.save(any(Faculty.class))).thenReturn(expected);
         // подготовка актуального результата
-        Faculty actual = schoolServiceImpl.update(expected);
+        Faculty actual = schoolServiceImpl.update(new Faculty(1, "No Griffindor", "Brown"));
         // тест
         assertEquals(expected, actual);
-        assertEquals(expected, schoolServiceImpl.get(1));
+        verify(facultysRepository, times(1)).save(any(Faculty.class));
+        verify(facultysRepository, times(1)).existsById(anyLong());
+
+    }
+
+    @Test
+    @DisplayName("Исключение при редактировании если нет такого факультета")
+    public void throwWhenTryUpdateFacultyIfNotExsist() {
+        // подготовка ожидаесого результата
+        Faculty expected = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
+        when(facultysRepository.existsById(anyLong())).thenReturn(false);
+        // подготовка актуального результата
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> schoolServiceImpl.update(expected));
+        // тест
+        assertEquals("Такого факультута нет в БД", thrown.getMessage());
+        verify(facultysRepository, times(1)).existsById(anyLong());
+        verify(facultysRepository, times(0)).save(any(Faculty.class));
     }
 
     @Test
     @DisplayName("Получить факультеты по цвету")
-    public void getFacultysByColor() {
+    public void getCollectionsFacultysByColor() {
         // подготовка ожидаемого результата
-        Map<Long, Faculty> expected = new HashMap<>();
-        Faculty faculty = DEFAULT_MAPS_OF_FACULTY.get((long) 1);
-        expected.put((long) 1, faculty);
+        Collection<Faculty> expected = new ArrayList<>();
+        expected.add(DEFAULT_MAPS_OF_FACULTY.get((long) 1));
+        expected.add(DEFAULT_MAPS_OF_FACULTY.get((long) 2));
+        when(facultysRepository.findAllByColor("Red")).thenReturn(expected);
         // подготовка актуального результата
-        Map<Long, Faculty> actual = schoolServiceImpl.byFilter("Red");
+        Collection<Faculty> actual = schoolServiceImpl.filterByColor("Red");
         // тест
         assertEquals(expected, actual);
+        verify(facultysRepository, times(1)).findAllByColor("Red");
     }
-
-    @Test
-    @DisplayName("Исключение если фильтрация не по строке")
-    public void thrownErrorIfIlliegalArgsInFilterMethod() {
-        Integer arg = 1;
-        String errorMsg = String.format("метод byFilter ожидает строку. Передан %s", arg.getClass());
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
-                () -> schoolServiceImpl.byFilter(arg));
-        assertEquals(errorMsg, thrown.getMessage());
-    }
-    // подготовка ожидаемого результата
-    // подготовка актуального результата
-    // тест
 }
--- src/test/java/ru/hogwarts/school/service/StudentSchoolServiceImplTest.java
+++ src/test/java/ru/hogwarts/school/service/StudentSchoolServiceImplTest.java
@@ -3,17 +3,29 @@ package ru.hogwarts.school.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentsRepository;
 import ru.hogwarts.school.service.implementation.StudentSchoolServiceImpl;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class StudentSchoolServiceImplTest {
-    private final StudentSchoolServiceImpl schoolServiceImpl = new StudentSchoolServiceImpl();
     private final Map<Long, Student> DEFAULT_MAPS_OF_STUDENT = new HashMap<>();
+    @Mock
+    private StudentsRepository studentsRepository;
+    @InjectMocks
+    private StudentSchoolServiceImpl schoolServiceImpl;
 
     @BeforeEach
     void set_up() {
@@ -21,8 +33,6 @@ public class StudentSchoolServiceImplTest {
         DEFAULT_MAPS_OF_STUDENT.put((long) 2, new Student(2, 20, "Ron"));
         DEFAULT_MAPS_OF_STUDENT.put((long) 3, new Student(3, 18, "Hermiona"));
         DEFAULT_MAPS_OF_STUDENT.put((long) 4, new Student(4, 20, "Luna"));
-
-        schoolServiceImpl.create(DEFAULT_MAPS_OF_STUDENT.get((long) 1));
     }
 
 
@@ -31,11 +41,12 @@ public class StudentSchoolServiceImplTest {
     public void createFaculty() {
         // подготовка ожидаемого результата
         Student expected = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+        when(studentsRepository.save(any(Student.class))).thenReturn(expected);
         // подготовка актуального результата
         Student actual = schoolServiceImpl.create(expected);
         // тест
         assertEquals(expected, actual);
-
+        verify(studentsRepository, times(1)).save(any(Student.class));
     }
 
     @Test
@@ -43,56 +54,97 @@ public class StudentSchoolServiceImplTest {
     public void removeFacyltu() {
         // подготовка ожидаемого результата
         Student expected = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+        when(studentsRepository.existsById(anyLong())).thenReturn(true);
+        doNothing().when(studentsRepository).deleteById(anyLong());
         // подготовка актуального результата
-        Student actual = schoolServiceImpl.remove(1);
+        schoolServiceImpl.remove(anyLong());
         // тест
-        assertEquals(expected, actual);
-        assertNull(schoolServiceImpl.get(1));
+        verify(studentsRepository, times(1)).existsById(anyLong());
+        verify(studentsRepository, times(1)).deleteById(anyLong());
     }
+
+    @Test
+    @DisplayName("Исключение при удалении если нет такого студента")
+    public void throwWhenTryRemoveFacultyIfNotExsist() {
+        // подготовка ожидаесого результата
+        Student expected = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+        when(studentsRepository.existsById(anyLong())).thenReturn(false);
+        // подготовка актуального результата
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> schoolServiceImpl.remove(anyLong()));
+        // тест
+        assertEquals("Такого студента нет в БД", thrown.getMessage());
+        verify(studentsRepository, times(1)).existsById(anyLong());
+        verify(studentsRepository, times(0)).deleteById(anyLong());
+    }
+
 
     @Test
     @DisplayName("Получить студента по индексу")
     public void getFacultyById() {
         // подготовка ожидаемого результата
         Student expected = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+        when(studentsRepository.findById(anyLong())).thenReturn(Optional.of(expected));
         // подготовка актуального результата
         Student actual = schoolServiceImpl.get(1);
         // тест
         assertEquals(expected, actual);
+        verify(studentsRepository, times(1)).findById(anyLong());
     }
 
     @Test
     @DisplayName("Редактировать студента")
     public void editFaculty() {
         // подготовка ожидаемого результата
-        Student expected = new Student(1, 100, "Brown");
+        Student expected = new Student(1, 19, "Brown");
+        when(studentsRepository.existsById(anyLong())).thenReturn(true);
+        when(studentsRepository.save(any(Student.class))).thenReturn(expected);
         // подготовка актуального результата
-        Student actual = schoolServiceImpl.update(expected);
+        Student actual = schoolServiceImpl.update(new Student(1, 19, "Brown"));
         // тест
         assertEquals(expected, actual);
-        assertEquals(expected, schoolServiceImpl.get(1));
+        verify(studentsRepository, times(1)).save(any(Student.class));
+        verify(studentsRepository, times(1)).existsById(anyLong());
+
+    }
+    @Test
+    @DisplayName("Исключение при редактировании если нет такого студента")
+    public void throwWhenTryUpdateFacultyIfNotExsist() {
+        // подготовка ожидаесого результата
+        Student expected = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+        when(studentsRepository.existsById(anyLong())).thenReturn(false);
+        // подготовка актуального результата
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> schoolServiceImpl.update(expected));
+        // тест
+        assertEquals("Такого студента нет в БД", thrown.getMessage());
+        verify(studentsRepository, times(1)).existsById(anyLong());
+        verify(studentsRepository, times(0)).save(any(Student.class));
     }
 
     @Test
     @DisplayName("Получить студентов по возрасту")
     public void getFacultysByColor() {
-        // подготовка ожидаемого результата
-        Map<Long, Student> expected = new HashMap<>();
-        Student student = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
-        expected.put((long) 1, student);
-        // подготовка актуального результата
-        Map<Long, Student> actual = schoolServiceImpl.byFilter(18);
-        // тест
-        assertEquals(expected, actual);
+//        // подготовка ожидаемого результата
+//        Map<Long, Student> expected = new HashMap<>();
+//        Student student = DEFAULT_MAPS_OF_STUDENT.get((long) 1);
+//        expected.put((long) 1, student);
+//        // подготовка актуального результата
+//        Map<Long, Student> actual = schoolServiceImpl.byFilter(18);
+//        // тест
+//        assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Исключение если фильтрация не по целочисленному аргументу")
-    public void thrownErrorIfIlliegalArgsInFilterMethod() {
-        String arg = "1";
-        String errorMsg = String.format("метод byFilter ожидает int. Передан %s", arg.getClass());
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
-                () -> schoolServiceImpl.byFilter(arg));
-        assertEquals(errorMsg, thrown.getMessage());
+    @DisplayName("Получить студентов по возрасту")
+    public void getCollectionsFacultysByColor() {
+        // подготовка ожидаемого результата
+        Collection<Student> expected = new ArrayList<>();
+        expected.add(DEFAULT_MAPS_OF_STUDENT.get((long) 2));
+        expected.add(DEFAULT_MAPS_OF_STUDENT.get((long) 4));
+        when(studentsRepository.findAllByAge(20)).thenReturn(expected);
+        // подготовка актуального результата
+        Collection<Student> actual = schoolServiceImpl.filterByAge(20);
+        // тест
+        assertEquals(expected, actual);
+        verify(studentsRepository, times(1)).findAllByAge(20);
     }
 }
